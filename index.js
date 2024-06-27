ad6ca27600e817d23aae4018eed9c3d11fbdb453
Simon Nguthu <simon.nguthu@student.moringaschool.com>
2024-06-27 17:58:07 +0300

    loops

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(array, event){
+  const messages = [];
+  for(let i = 0; i < array.length; i++) {
+    const message = `Thank you, ${array[i]}, for the wonderful ${event} gift!`
+    messages.push(message);
+  }
+  return messages;
+}
+function countDown(n){
+  let i = n;
+  while(i >= 0){
+      console.log(i--);
+  }
+}
